70ae5edf9363324b67c809216a883df90ff5ca48
ldev <ldev@ldev.eu.org>
2023-11-12 13:50:31 +0100

    changed some stuff and test.sh

--- testing/test.sh
+++ testing/test.sh
@@ -4,15 +4,72 @@ bin_path="../target/debug/git-credential-pass"
 
 payload=$'username=ldev\nhost=git.ldev.eu.org\npassword=passwd\n'
 
+echo "DELETING dev password dir"
+pass rm dev
+
+echo "READY FOR TEST"
+echo ""
 
 run_test()
 {
-  echo "TEST $0"
-  echo "$payload" | $bin_path -vvv -t $1 -p $2 $3
+  echo ""
+  echo "RUNNING $1"
+  output=$(echo "$payload" | $bin_path -vvv -t $2 -p $3 $4)
 
-  if [ $? != $4 ] ; then
-    echo "TEST FAILED expected exitcode: $4"
+  if [ $? != $5 ] ; then
+    echo "TEST FAILED: exit code: $? expected exitcode: $5"
     exit -1
   fi
+
+  if [ "$output" != "$6" ] ; then
+    echo "TEST FAILED: expected output:"
+    echo "$6"
+    echo "Got output:"
+    echo "$output"
+    exit -1
+  fi
+  echo ""
 }
+
+test_store_ok(){
+  run_test "$1" "$2" "$3" store 0 "$4"
+}
+test_get_ok(){
+  run_test $1 $2 $3 get 0 "$4"
+}
+test_erase_ok(){
+  run_test $1 $2 $3 erase 0 "$4"
+}
+
+test_store_err(){
+  run_test "$1" "$2" "$3" store 1 "$4"
+}
+
+pass_exist(){
+  pass show $1
+  if [ $? != 0 ] ; then
+    echo "TEST FAILED: password not in store"
+  fi
+}
+pass_not_exist(){
+  pass show $1
+  if [ $? == 0 ] ; then
+    echo "TEST FAILED: password not deleted"
+  fi
+}
+
+tmpls="templates"
+
+test_store_ok "simple_store" "$tmpls/default.template" "dev/{host}" ""
+pass_exist "dev/git.ldev.eu.org"
+test_get_ok "simple_get" "$tmpls/default.template" "dev/{host}" $'password=passwd\nusername=ldev'
+test_erase_ok "simple_erase" "$tmpls/default.template" "dev/{host}" ""
+pass_not_exist "dev/git.ldev.eu.org"
+
+test_store_err "invalid_token_order" "$tmpls/invalidtokenorder.template" "dev/invalid_token_order" ""
+
+echo "ALL TESTS PASSED"
+exit 0
+
+
 
