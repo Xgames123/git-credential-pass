3068267ac93168235a733e0a45c202aebcfb8387
ldev <ldev@ldev.eu.org>
2023-12-05 22:35:09 +0100

    verify things

--- verify.sh
+++ verify.sh
@@ -2,11 +2,20 @@
 
 
 cargo_ver=$(rg -or "\$1" "^version[\t ]*=[\t ]*\"([0-9]\.[0-9]\.[0-9])\"" Cargo.toml)
-echo "$cargo_ver"
+echo "version: $cargo_ver"
 pkg_ver=$(rg -or "\$1" "^pkgver=[\t ]*([0-9]\.[0-9]\.[0-9])" packaging/PKGBUILD)
+
+git_ver=$(git tag | tail -n 1)
 
 
 if [ "$cargo_ver" != "$pkg_ver" ] ; then
-  echo "Cargo.toml version: $cargo_ver doesn't match package version: $pkg_ver"
+  echo "PKGBUILD version '$pkg_ver' doesn't match $cargo_ver"
   exit -1
 fi
+
+if [ "$git_ver" != "$cargo_ver" ] ; then
+  echo "git tag '$git_ver' doesn't match version $cargo_ver"
+  exit -1
+fi
+
+echo "ALL CHECKS OK"
